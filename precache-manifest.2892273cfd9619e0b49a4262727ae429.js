66b31dc199e8576541e668ddd860b2592848dcbe
ramirocastillo93 <19273823+ramirocastillo93@users.noreply.github.com>
2020-04-20 01:41:27 -0400

    Updates

---
+++ precache-manifest.2892273cfd9619e0b49a4262727ae429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cc5c51a8be7e8703b4444ae6d7084a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "279fa0132dbc66dfcd8a",
+    "url": "/static/css/main.e386ac7f.chunk.css"
+  },
+  {
+    "revision": "2d9efc145baad1c94513",
+    "url": "/static/js/2.4f0af9db.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/static/js/2.4f0af9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279fa0132dbc66dfcd8a",
+    "url": "/static/js/main.12137ce2.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
